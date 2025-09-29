4df76c5b3b5a575692780337cb275e41845615bf
mangeshvzt <mangeshv@zignuts.com>
2025-09-29 15:27:49 +0530

    testing

--- deploy_prefect.sh
+++ deploy_prefect.sh
@@ -44,9 +44,17 @@ fi
 
 # echo "Prefect deployment completed for environment: $ENV_TAG"
 
-# Deploy flow
-prefect deployment create flows/hello_world.py:hello_world \
-    --name hello-world-deployment \
-    --apply
+# # Deploy flow
+# prefect deployment create flows/hello_world.py:hello_world \
+#     --name hello-world-deployment \
+#     --apply
 
-echo "Prefect deployment completed for environment: $ENV_TAG"
+# echo "Prefect deployment completed for environment: $ENV_TAG"
+
+prefect deployment build hello_world.py:hello_flow \
+    -n hello-world \
+    -i $IMAGE_TAG \
+    -q default \
+    -o hello_world_deployment.yaml
+
+prefect deployment apply hello_world_deployment.yaml
